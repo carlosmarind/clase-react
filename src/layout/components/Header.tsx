8bafc21716cc43ede7a01cc76773ed680a68d718
Carlos Marin Diaz <carlosmarind@gmail.com>
2024-09-28 10:39:39 -0300

    render condicionado de enlace

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom"
 import { LoginButton } from "../../components/LoginButton"
 import styles from "./Header.module.css"
+import { useEffect, useState } from "react";
+import { isAuth } from "../../services/login/loginService";
 
 export const Header = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+    useEffect(() => {
+        setIsLoggedIn(isAuth())
+    }, []);
+
     return (
         <header className={styles.header}>
             <ul className={styles.links}>
                 <li><Link className={styles.link} to="/">Home</Link></li>
                 <li><Link className={styles.link} to="/personajes">Personajes</Link></li>
-                <li><Link className={styles.link} to="/dashboard">Dashboard</Link></li>
+                { !isLoggedIn && <li><Link className={styles.link} to="/dashboard">Dashboard</Link></li> }
                 <li><Link className={styles.link} to="/posts">Posts</Link></li>
                 <li><Link className={styles.link} to="/admin">Admin</Link></li>
                 <li><Link className={styles.link} to="/about">About</Link></li>
