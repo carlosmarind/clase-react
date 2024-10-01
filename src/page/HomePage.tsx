39ca82066874eba0caa19a76f62d8b3d72c2bbdb
Carlos Marin Diaz <carlosmarind@gmail.com>
2024-10-01 16:35:36 -0300

    simplificacion del home

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,36 +1,6 @@
-import { useEffect } from "react"
 import { MainLayout } from "../layout/MainLayout"
 
 export const HomePage = () => {
-
-    useEffect(() => {
-
-        const fectchData = async () => {
-            //primero
-            console.log('iniciando solicitud web')
-            const fetchOptions = {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body : JSON.stringify({
-                    name: 'jose',
-                    age: 25
-                })
-            }
-            try {
-                const response = await fetch('http://localhost:4000/posts', fetchOptions)
-                const data = await response.json();
-                console.log(data)
-            } catch (error) {
-                console.log('error en la solicitud', error)
-            }
-            //quinto
-            console.log('terminando solicitud web')
-        }
-        fectchData()
-
-    }, []);
 
     return (
         <MainLayout>
